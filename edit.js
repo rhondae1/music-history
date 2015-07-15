8622e774f70e32e17969d31756a96123425377c8
Rhonda Eisenhauer <rhonda.s.eisenhauer@live.com>
2015-07-15 09:11:06 -0500

    music history project

---
+++ edit.js
@@ -0,0 +1,7 @@
+var songs = [];
+
+songs[songs.length] = "Legs by ZZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertramp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welcome to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironic > by Alanis Morisette on the album Jagged Little Pill";
